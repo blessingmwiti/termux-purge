d10361379de22bf86783b4aa4d07fcf429a76245
BlessingMwiti <88279683+blessingmwiti@users.noreply.github.com>
2022-09-17 20:20:03 +0300

    Delete install.sh

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/sh
-echo "This will copy purge file to your Termux PATH"
-chmod +x purge
-cp purge /data/data/com.termux/files/usr/bin
-echo "File copied successfully"
-echo "Now just type purge and enter the name of the package to be removed"
-echo "Read README file for usage!"
